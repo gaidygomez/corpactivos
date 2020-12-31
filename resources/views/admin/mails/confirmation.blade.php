fc927992902710642f39da1f823f73d71b5d2ee4
Gaidy Gomez <gggomez20@gmail.com>
2020-12-31 17:57:20 -0430

    Añadido el logo al email de confirmación

--- resources/views/admin/mails/confirmation.blade.php
+++ resources/views/admin/mails/confirmation.blade.php
@@ -7,9 +7,12 @@
     <title>Confirmation Email</title>
 </head>
 <body>
+	<div style="display: flex; justify-content: center; padding: 2rem;">
+		<img src="{{ asset('img/logo.jpg') }}" width="100" height="100">
+	</div>
     <h1 style="text-align: center; padding: 1rem;">Su Transacción ha sido aprobada</h1>
 
     <p style="text-align: justify; padding: 1rem;">Estimado {{ $deposit->user->fname }} {{ $deposit->user->lname }} </p>
-    <p style="text-align: justify; padding: 1rem;">El déposito que usted ha realizado con número de voucher <strong>{{ $deposit->voucher }}</strong> con cuenta destino <strong>{{ $deposit->ban }}</strong> ha sido aprobada.</p>
+    <p style="text-align: justify; padding: 1rem;">El déposito que usted ha realizado con número de voucher <strong>{{ $deposit->voucher }}</strong> con cuenta destino <strong>{{ $deposit->ban }}</strong> que recibirá Bs <strong> {{ number_format($amount, 2, ',', '.') }} </strong> ha sido aprobada.</p>
 </body>
 </html>
